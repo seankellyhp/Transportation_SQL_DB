a425023e315b232a3f6c7509c227eee94d0105ef
seankellyhp <seankellyhp@gmail.com>
2020-06-09 23:07:48 +0200

    Update Create_Users.sql

--- Create_Users.sql
+++ Create_Users.sql
@@ -1,12 +1,8 @@
 
 CREATE USER readonlylog FROM LOGIN readonlylog ; 
-EXEC sp_addrolemember 'db_datareader', readonlylog; 
 
 CREATE USER adminlog FROM LOGIN adminlog ; 
-EXEC sp_addrolemember 'db_securityadmin', adminlog;
 
 CREATE USER managerlog FROM LOGIN managerlog ; 
-EXEC sp_addrolemember 'db_accessadmin', managerlog;
 
 CREATE USER userlog FROM LOGIN userlog ; 
-EXEC sp_addrolemember 'db_datawriter', userlog;
